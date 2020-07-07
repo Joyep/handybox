78f536f9be1a9653cc6199a91c94a9ef50a46d31
yudingp <yudingp@163.com>
2020-07-07 19:16:50 +0800

    hand/git/st: Optimize2

--- hand/git/st.sh
+++ hand/git/st.sh
@@ -1,7 +1,14 @@
 hand_git_st()
 {
-    cd $1
-    hand echo green `pwd`
-    hand echo do git status
-    cd - > /dev/null
+    if [ $# -eq 0 ]; then
+        git status
+        return $?
+    fi
+    local i
+    for i in $* ; do
+        cd $i
+        hand echo green `pwd`
+        hand echo do git status
+        cd - > /dev/null
+    done
 }
